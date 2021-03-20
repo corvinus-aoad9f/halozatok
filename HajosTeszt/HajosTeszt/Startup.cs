034da7ff27896e5b60dff17538755b10267042ab
corvinus-aoad9f <peter.tolvaj@stud.uni-corvinus.hu>
2021-03-20 23:25:39 +0100

    hatodik gyak

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -24,7 +24,7 @@ namespace HajosTeszt
             app.UseHttpsRedirection();
             DefaultFilesOptions options = new DefaultFilesOptions();
             options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("hajo.html");
+            options.DefaultFileNames.Add("jsgyak.html");
             app.UseDefaultFiles(options);
             app.UseStaticFiles();
             //app.UseDefaultFiles();
---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,36 @@
+﻿window.onload = function () {
+    console.log("geráppá")
+    var elso = document.getElementById("elso");
+    var sor = document.createElement("div");
+    sor.classList.add("sor");
+    elso.appendChild(sor);
+    for (var i = 0; i < 10; i++) {
+        var szam = document.createElement("div");
+        sor.appendChild(szam);
+        szam.classList.add("elem");
+        szam.innerText = i + 1;
+        szam.style.background = `rgb(255, ${255 - 20 * i}, 255)`;
+    }
+
+    var faktoriális = function (n) {
+        let er = 1;
+        for (let i = 2; i <= n; i++) {
+            er = er * i;
+        }
+        return er;
+    }
+
+    for (var s = 0; s < 10; s++) {
+        var pascal = document.getElementById("pascal");
+        var sorr = document.createElement("div");
+        sorr.classList.add("sor");
+        pascal.appendChild(sorr);
+        for (var o = 0; o <= s; o++) {
+
+            var elem = document.createElement("div");
+            elem.classList.add("elem");
+            elem.innerText = faktoriális(s) / (faktoriális(o) * faktoriális(s - o));
+            sorr.appendChild(elem);
+        }
+    }
+}
