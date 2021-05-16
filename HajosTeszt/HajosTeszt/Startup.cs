808010af7170cfa44634d044cc37e6e0cffb1c36
corvinus-aoad9f <peter.tolvaj@stud.uni-corvinus.hu>
2021-05-16 20:31:32 +0200

    utolsó gyak

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -29,7 +29,7 @@ namespace HajosTeszt
             app.UseHttpsRedirection();
             DefaultFilesOptions options = new DefaultFilesOptions();
             options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("hajo.html");
+            options.DefaultFileNames.Add("jokes.html");
             app.UseDefaultFiles(options);
             app.UseStaticFiles();
             //app.UseDefaultFiles();
